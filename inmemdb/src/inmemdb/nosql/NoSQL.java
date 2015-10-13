ee38ae01a503d9b2a6b062c832f97cb93a9b1a1b
Xcreed <jpm311296@gmail.com>
2015-10-12 18:30:56 -0600

    Tables and indexes?? Design pattern

--- inmemdb/src/inmemdb/nosql/NoSQL.java
+++ inmemdb/src/inmemdb/nosql/NoSQL.java
@@ -1,5 +1,15 @@
 package inmemdb.nosql;
 
-public class NoSQL {
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+
+public class NoSQL extends DB {
+
+	public NoSQL(Mongo mongo, String name) {
+		super(mongo, name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 }
