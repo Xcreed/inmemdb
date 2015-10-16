9c252fd34cc46d2a9c39369ced92555309eac56e
Xcreed <jpm311296@gmail.com>
2015-10-16 00:31:30 -0600

    Fixed

---
+++ src/inmemdb/ceRobot/Main.java
@@ -0,0 +1,10 @@
+package ceRobot;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/inmemdb/nosql/Main.java
@@ -0,0 +1,10 @@
+package inmemdb.nosql;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		new Schema("Clase");
+	}
+
+}
