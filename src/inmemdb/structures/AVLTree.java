d000071ab45a508a614e726163b14b7061f0c4e8
morazan <morazan@monge>
2015-10-28 16:37:56 -0600

    Merged master

--- src/inmemdb/structures/AVLTree.java
+++ src/inmemdb/structures/AVLTree.java
@@ -1,7 +1,7 @@
 package inmemdb.structures;
 
 
-public class AVLTree <T> extends Tree{
+public class AVLTree <T>{
 	public AVLNode root; 
 	public int index;
 
@@ -329,19 +329,4 @@ public class AVLTree <T> extends Tree{
 		}
 	}
 
-	public String getDataString() {
-		return getDataStringAux(root);
-	}
-	
-	private String getDataStringAux(AVLNode current) {
-		StringBuilder list = new StringBuilder();
-		
-		if(current != null){
-			getDataStringAux(current.leftChild);
-			list.append("Data: "+current+"--Key: "+current.key);
-			getDataStringAux(current.rightChild);
-		} 
-		System.out.println(list);
-		return list.toString();
-	}
 }
